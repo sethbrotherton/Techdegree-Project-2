e1f965e593dfdc36ce9d8fe1fbe3c653d2601376
sethbrotherton <sethbrotherton@yahoo.com>
2018-07-24 03:27:42 -0400

    added comments

--- pagination.js
+++ pagination.js
@@ -1,5 +1,5 @@
 
-
+// Divides lists into 10s, displaying one 10s at a time
 function showPage(pageNumber, list) {
   for (var i = 0; i < list.length; i++) {
     list[i].style.display = 'none';
@@ -10,15 +10,18 @@ function showPage(pageNumber, list) {
     }
   }
 }
+
+// making a list, and calling the showPage function with it
 var studentList = $('.student-item');
 showPage(1, studentList);
 
+// creating an unordered list of pagination links and appending them
 function appendPageLinks(list) {
   var paginationDiv = document.createElement('div');
   paginationDiv.classList.add('pagination');
   var paginationUl = document.createElement('ul');
   paginationDiv.appendChild(paginationUl);
-
+// looping through how many groups of tens there are and creates a  clickable pagination link for each one
   for (var i = 0; i <= (list.length / 10); i++) {
     var paginationLi = document.createElement('li');
     paginationUl.appendChild(paginationLi);
@@ -34,6 +37,7 @@ function appendPageLinks(list) {
 }
 appendPageLinks(studentList);
 
+// dynamically adds a search input and button
 function addSearch() {
   var search = document.createElement('div');
   search.classList.add('student-search');
@@ -49,8 +53,10 @@ function addSearch() {
 }
 addSearch();
 
+
 var filteredStudents = [];
 var searchBar = $('.student-search');
+// adding event listener to search bar, to display matching results automatically
 $('.student-search').bind('keyup submit', function(e) {
   var entry = e.target.value.toLowerCase();
   var listOfStudentsDetails = $('li.student-item > .student-details');
@@ -70,14 +76,14 @@ $('.student-search').bind('keyup submit', function(e) {
   appendPageLinks(filteredStudents);
   noResultsFunc();
 });
+$('.page-header').prepend(searchBar);
 
+// appends a no results message if there are no matches to search input
 function noResultsFunc() {
   if(filteredStudents.length === 0) {
     var noResults = $('<h1></h1>').text('Sorry, no results were found.').addClass('noResultsDisplay');
-    $('.page-header').append(noResults);
+    $('.student-list').prepend(noResults);
   } else {
     $('.noResultsDisplay').remove();
   }
 }
-
-$('.page-header').prepend(searchBar);
